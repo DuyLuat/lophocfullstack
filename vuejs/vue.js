7ae3c75e89af69846c6f3916bd0a9b47b799be43
DuyLuat <daoduyluat@gmail.com>
2019-01-23 22:26:50 +0700

    Học VueJS

---
+++ vuejs/vue.js
@@ -0,0 +1,22 @@
+var per1son=new Vue({
+    el : '#student-manager',
+data:{
+   students:[{
+       showEdit:false,
+   name:'Bùi Văn Nguyện',
+   age:30,
+   address:'cát bi, hải an, hải phòng'
+   },
+   {
+      showEdit:false,
+      name:'Bùi Văn Long',
+      age:3,
+      address:'cát bi, hải an,'
+      }
+
+   ]
+  
+    
+}
+}
+);
